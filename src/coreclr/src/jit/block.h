b8b828adb86594b0fe979b1f3998f31c8f769f8d
Kunal Pathak <Kunal.Pathak@microsoft.com>
2020-10-08 15:37:43 -0700

    Detect inner loop and add 10 bytes of padding at the beginning

--- src/coreclr/src/jit/block.h
+++ src/coreclr/src/jit/block.h
@@ -445,6 +445,7 @@ struct BasicBlock : private LIR::Range
 #define BBF_BACKWARD_JUMP_TARGET          0x1000000000 // Block is a target of a backward jump
 #define BBF_PATCHPOINT                    0x2000000000 // Block is a patchpoint
 #define BBF_HAS_SUPPRESSGC_CALL           0x4000000000 // BB contains a call to a method with SuppressGCTransitionAttribute
+#define BBF_FIRST_BLOCK_IN_INNERLOOP      0x8000000000 // Block is lexically the fist block within the innermost loop.
 
 // clang-format on
 

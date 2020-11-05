924d8deab7a628a20a3e6e4417c6dfe91b8efcc6
Kunal Pathak <Kunal.Pathak@microsoft.com>
2020-11-05 10:44:36 -0800

    Added JitAlignLoopAdaptive algorithm

--- src/coreclr/src/jit/compiler.cpp
+++ src/coreclr/src/jit/compiler.cpp
@@ -2537,6 +2537,9 @@ void Compiler::compInitOptions(JitFlags* jitFlags)
     compJitAlignLoopMaxCodeSize    = JitConfig.JitAlignLoopMaxCodeSize();
     compJitAlignLoopBoundary       = ReinterpretHexAsDecimal(JitConfig.JitAlignLoopBoundary());
     compJitAlignLoopForJcc         = JitConfig.JitAlignLoopForJcc() == 1;
+    compJitAlignLoopAdaptive       = JitConfig.JitAlignLoopAdaptive() == 1;
+    assert(isPow2(compJitAlignLoopBoundary));
+
 
 #if REGEN_SHORTCUTS || REGEN_CALLPAT
     // We never want to have debugging enabled when regenerating GC encoding patterns
@@ -3849,6 +3852,7 @@ _SetMinOpts:
         else
         {
             codeGen->SetAlignLoops(opts.jitFlags->IsSet(JitFlags::JIT_FLAG_ALIGN_LOOPS));
+            //codeGen->SetAlignLoops(true);
         }
     }
 

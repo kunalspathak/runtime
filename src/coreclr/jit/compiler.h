c2b5a1ee30bfa557906363a4e78033b63c36f1e5
Kunal Pathak <Kunal.Pathak@microsoft.com>
2024-12-31 13:41:29 -0800

    getVectorTByteLength() return different value

--- src/coreclr/jit/compiler.h
+++ src/coreclr/jit/compiler.h
@@ -9363,6 +9363,10 @@ public:
             return XMM_REGSIZE_BYTES;
         }
 #elif defined(TARGET_ARM64)
+        if (compExactlyDependsOn(InstructionSet_Sve_Arm64))
+        {
+            return 32; // This should call GetSveLengthFromOS()
+        }
         if (compExactlyDependsOn(InstructionSet_VectorT128))
         {
             return FP_REGSIZE_BYTES;
